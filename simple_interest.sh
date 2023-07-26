f31556c9255baeddbf508408b74d44fdb9cd8455
FrederickvPaulov <109754890+FrederickvPaulov@users.noreply.github.com>
2023-07-25 21:00:31 -0500

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
+# This  code  estimates  simple interest given principal,
+# annual rate of interest and time period in years.
 
-# Function to calculate simple interest
-calculate_simple_interest() {
-    principal=$1
-    rate=$2
-    time=$3
 
-    interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
-    echo $interest
-}
 
-# Main script starts here
+# Author:  Frederickv Albornoz Escobedo
 
-# Get user input
-read -p "Enter the principal amount: " principal_amount
-read -p "Enter the interest rate (in percentage): " interest_rate
-read -p "Enter the time period (in years): " time_period
+# A name to be considered 
 
-# Calculate simple interest using the function
-interest=$(calculate_simple_interest $principal_amount $interest_rate $time_period)
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
 
-# Display the result
-echo "Simple Interest: $interest"
+# Output:
+# simple interest = p*t*r
+
+echo"Enter the principal value :"
+read p    
+echo"Enter rate of interest per year actual value :"
+read r    
+echo"Enter time period in years :"
+read t     
+
+
+#  actual opearation to be accountable for  : 
+s=`expr $p \* $t \* $r / 100`    
+echo"The simple interest is: "
+echo$s
